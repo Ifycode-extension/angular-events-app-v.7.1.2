1a970b6e19f7d02eb5af1e067bedd6077c43ce3e
ifycode <marychord@gmail.com>
2020-09-22 21:47:05 -0800

    Template variable - part (use case) 2

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -19,6 +19,8 @@ import { Component, Input } from '@angular/core'
 export class EventThumbnailComponent {
   @Input() event:any
 
+  someProperty:any = 'some value'
+
   logFoo() {
     console.log('foo')
   }
